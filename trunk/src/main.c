17582258b798bdf8588001bdbc34902000c08461
b100dian <b100dian@6bdb3c60-4643-0410-86bb-99cbaf4d9aa6>
2008-01-16 23:00:56 +0000

    chantier

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -35,31 +35,24 @@
 
 int main( int argc, char *argv[] )
 {
-	gboolean no_valac = 0;
-	gboolean keep_output = 0;
 	gboolean version = 0;
 	gchar * output_file = 0;
 	gchar * output_file_basename = 0;
 	gchar * input_file = 0;
-	gchar * valac_path;
 	GMappedFile * mapped_input;
 
 	/* command-line arguments parsing */
 	GOptionEntry entries[] = 
 	{
-		{ "keep-output", 0, 0, G_OPTION_ARG_NONE, &keep_output, "does not erase the intermediate .vala file", 0 },
-		{ "output-file", 0, 0, G_OPTION_ARG_FILENAME, &output_file, "specify the name of the output .vala file", 0 },
-		{ "no-valac", 0, 0, G_OPTION_ARG_NONE, &no_valac, "don't run valac on resulting file. Implies --keep-output.", 0 },
-		{ "with-valac", 0, 0, G_OPTION_ARG_FILENAME, &valac_path, "path to valac binary", 0 },
-		{ "version", 0, 0, G_OPTION_ARG_NONE, &version, "prints version and exit", 0 },
+		{ "output-file", 'o', 0, G_OPTION_ARG_FILENAME, &output_file, "specify the name of the output .vala file", "OUTPUT_FILE" },
+		{ "version"    , 'v', 0, G_OPTION_ARG_NONE    , &version, "prints version and exits", 0 },
 		{ NULL }
 	};	
 	GOptionContext *context = 0;
 	GError * error = 0;
 
-	context = g_option_context_new (" [VALAC OPTIONS] FILE");
+	context = g_option_context_new (" INPUT_FILE");
 	g_option_context_add_main_entries (context, entries, 0);
-	g_option_context_set_ignore_unknown_options (context, 1);
 	g_option_context_set_summary(context, "Gtkaml is an xml preprocessor that outputs Gtk+ Vala source code");
   	g_option_context_parse (context, &argc, &argv, &error);
   	g_option_context_free(context);
@@ -74,8 +67,7 @@ int main( int argc, char *argv[] )
 		fprintf(stdout,"Gtkaml " VERSION "\n");
 		return 0;
 	}
-	if (no_valac) keep_output = 1;
-	if (argc < 2|| argv[argc-1][0]=='-' )
+	if (argc < 2 || argv[argc-1][0]=='-' )
 	{
 		fprintf(stderr, "No input file specified\n");
 		return 1;
@@ -90,19 +82,12 @@ int main( int argc, char *argv[] )
 	{
 		if (g_strrstr(input_file, ".")) {
 			output_file_basename = g_strndup(input_file, g_strrstr(input_file, ".") - input_file);
-			output_file = g_strconcat( output_file_basename, ".vala", 0 );
+			output_file = g_strconcat( output_file_basename, ".vala", NULL );
 			g_free( output_file_basename );
 		} else {
-			output_file = g_strconcat( input_file, ".vala", 0 );
+			output_file = g_strconcat( input_file, ".vala", NULL );
 		}
 	}
-	
-	if (!valac_path)
-		valac_path = g_strdup(g_find_program_in_path("valac"));
-	
-	//stripping the arguments, for valac
-	argv+=1; //the executable path: shift one to the right
-	argc-=2; //the executable path, plus the last (input_file) argument
 	
 	mapped_input = g_mapped_file_new( input_file, 0, &error );
 	if (error) {
@@ -124,7 +109,7 @@ int main( int argc, char *argv[] )
 	g_string_free( result, TRUE );
 	g_free( input_file );
 	g_free( output_file );
-	g_free( valac_path );
+
 	
 	return (0);
 }
